e3d15c344175d78bcc8d3a8418d8a7fd596b81e7
Caleb Elizondo <calebelizondo@tamu.edu>
2024-01-23 12:34:35 -0600

    uploaded data in csv format

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
-const sqlite3 = require('sqlite3');
+
 const app = require('express')();
 const PORT = 8080;
 
+
+//endpoint for specific program
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-
+//endpoint for all programs
+app.get('/all', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(
     PORT, 
