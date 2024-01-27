388e7b2550238c4854d61f8c4141f6e5b7d9549d
Caleb Elizondo <calebelizondo@tamu.edu>
2024-01-27 16:38:17 -0600

    set up .env vars

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,19 @@ const app = express();
 const sqlite3 = require('sqlite3').verbose();
 const PORT = 8080;
 const path = require('path');
+const ejs = require('ejs');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 //serve frontend
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './../frontend/index.html');
+
+    const args = {
+      BACKEND_URL: process.env.BACKEND_URL,
+    }
+    res.render(path.join(__dirname, '../frontend/index.ejs'), args);
 });
 
 // Endpoint for all programs
